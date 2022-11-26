9c7dec604dc3216d902ca2c81d3e6231dbb4af09
lehominhkhoa1006 <118983867+lehominhkhoa1006@users.noreply.github.com>
2022-11-26 21:05:55 +0700

    Updated Porsche Project
    - Display
    - Discount Code

--- Porsche.c
+++ Porsche.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
 
 struct DongXe {
     double gia_xe;
@@ -20,7 +21,7 @@ struct BaoHanhXe {
 double car_line(double current_total, struct DongXe cars[])
 {   
     int m;
-    printf("Danh sach nhung dong xe Porsche chung toi hien co:\n");
+    printf("\nDanh sach nhung dong xe Porsche chung toi hien co:\n");
     printf("1. Porsche Cayenne (Gia tieu chuan: 4.930.000.000 VND)\n");
     printf("2. Porsche Macan (Gia tieu chuan: 3.150.000.000 VND)\n");
     printf("3. Porsche Taycan (Gia tieu chuan: 4.170.000.000 VND)\n");
@@ -44,11 +45,16 @@ double car_line(double current_total, struct DongXe cars[])
 double paint(double current_total, struct MauXe paint_price[])
 {
     int n;
-    printf("Danh sach nhung mau sac chung toi hien co:\n");
+    printf("\nDanh sach nhung mau sac chung toi hien co:\n");
     printf("1. Den (Gia tieu chuan: 283.500.000 VND)\n");
     printf("2. Trang (Gia tieu chuan: 283.500.000 VND)\n");
     printf("3. Xam (Gia tieu chuan: 297.500.000 VND)\n");
     printf("4. Nau (Gia tieu chuan: 297.500.000 VND)\n");
+    printf("5. Kem (Gia tieu chuan: 297.500.000 VND)\n");
+    printf("6. Do (Gia tieu chuan: 318.280.225 VND)\n");
+    printf("7. Xanh (Gia tieu chuan: 318.280.225 VND)\n");
+    printf("8. Vang (Gia tieu chuan: 318.280.225 VND)\n");
+
     do {
         printf("Nhap vao 1 so dai dien cho mau xe ban muon mua:\n");
         scanf("%d", &n);
@@ -62,11 +68,12 @@ double paint(double current_total, struct MauXe paint_price[])
 double interior(double current_total, struct NoiThatXe interior_price[])
 {
     int o;
-    printf("Danh sach nhung loai noi that chung toi hien co:\n");
+    printf("\nDanh sach nhung loai noi that chung toi hien co:\n");
     printf("1. Low-class (So tien bo sung them: 0VND)\n");
     printf("2. Standard-class (So tien bo sung them: 25.000.000 VND)\n");
     printf("3. High-class (So tien bo sung them: 50.000.000 VND)\n");
     printf("4. Premium-class (So tien bo sung them: 100.000.000 VND)\n");
+
     do {
         printf("Nhap vao 1 so dai dien cho noi that ban muon mua:\n");
         scanf("%d", &o);
@@ -80,10 +87,10 @@ double interior(double current_total, struct NoiThatXe interior_price[])
 double insurance(double current_total, struct BaoHanhXe insurance_price[])
 {
     int p;
-    printf("Danh sach nhung goi bao hanh xe chung toi hien co:\n");
-    printf("1. Bao hanh mau son – 3 nam (Gia tieu chuan: 36.400.000 VND)\n");
-    printf("2. Bao hanh phu tung – 3 nam (Gia tieu chuan: 27.300.000 VND)\n");
-    printf("3. Bao hanh than vo – 3 nam (Gia tieu chuan: 81.900.000 VND)\n");
+    printf("\nDanh sach nhung goi bao hanh xe chung toi hien co:\n");
+    printf("1. Bao hanh mau son 3 nam (Gia tieu chuan: 36.400.000 VND)\n");
+    printf("2. Bao hanh phu tung 3 nam (Gia tieu chuan: 27.300.000 VND)\n");
+    printf("3. Bao hanh than vo 3 nam (Gia tieu chuan: 81.900.000 VND)\n");
     do {
         printf("Nhap vao 1 so dai dien cho goi bao hanh ban muon mua:\n");
         scanf("%d", &p);
@@ -95,6 +102,8 @@ double insurance(double current_total, struct BaoHanhXe insurance_price[])
 }
 int main()
 {
+    system("CLS");
+    char code_giam_gia[100];
     double total=0;
     struct DongXe cars[12];
     struct MauXe paint_price[10];
@@ -110,7 +119,6 @@ int main()
     {
         fscanf(f, "%lf", &buffer);
         cars[i].gia_xe= buffer;
-        printf("%f", cars[i].gia_xe);
     }
     fclose(f);
 
@@ -138,11 +146,19 @@ int main()
     }
     fclose(f);
 
+    printf("\nXin chao quy khach!\n");
     total = car_line(total,cars);
     total = paint(total,paint_price);
     total = interior(total,interior_price);
     total = insurance(total,insurance_price);
-
-    printf("Tong thiet hai cho vi tien cua ban la: %lf", total);
+    printf("\nNhap vao ma code giam gia: ");
+    scanf("%s", code_giam_gia);
+    if (!strcmp(code_giam_gia, "391875"))
+    {
+        printf("XIN CHUC MUNG! CODE GIAM GIA CUA BAN CHINH XAC!\n");
+        printf("BAN DUOC GIAM GIA 25 PHAN TRAM!\n");
+        total=total*0.75;
+    }
+    printf("\nTong thiet hai cho vi tien cua ban la: %lf", total);
     return 0;
 }
